176dd5c5664c39659a6adef9fdc60a201c13bb3c
Sadrawi <dr.sadrawi@gmail.com>
2025-10-03 22:16:05 +0700

    ok

--- app_food.py
+++ app_food.py
@@ -31,6 +31,40 @@ if uploaded_file is not None:
         st.image(seg_img, 
             caption="Segmentation Result", 
             use_container_width=True)
+        
+        r = results[0]
+
+        # Original image
+        orig_img = r.orig_img.copy()
+
+        # Get masks and classes
+        masks = r.masks.data.cpu().numpy()   # (N, H, W)
+        classes = r.boxes.cls.cpu().numpy().astype(int)
+
+        # Compute areas
+        plate_area, rice_area, chicken_area, vege_area, tahu_area, tempe_area  = 0, 0, 0, 0, 0, 0
+        for mask, cls in zip(masks, classes):
+            area = mask.sum()
+            if cls == 0:
+                plate_area+= area
+            elif cls == 1:
+                rice_area += area
+            elif cls == 2:
+                chicken_area += area
+            elif cls == 3:
+                vege_area += area
+            elif cls == 4:
+                tahu_area += area
+            elif cls == 5:
+                tempe_area += area
+
+        ALLplate = plate_area + rice_area + chicken_area + vege_area + tahu_area + tempe_area
+        print(f"Rice area: {(rice_area/ALLplate)*100:5.2f} %")
+        print(f'Chicken area: {(chicken_area/ALLplate)*100:5.2f} %')
+        print(f'Vege area: {(vege_area/ALLplate)*100:5.2f} %')
+        print(f'Tahu area: {(tahu_area/ALLplate)*100:5.2f} %')
+        print(f'Tempe area: {(tempe_area/ALLplate)*100:5.2f} %')
+        st.write(ALLplate)
     # # Convert image for YOLO
     # img_array = np.array(image)
     # # if st.button("Run Segmentation"):
